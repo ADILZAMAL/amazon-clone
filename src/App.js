85ccf888f0ddeb5b92d0b2ce5e9d05974564eba0
ADILZAMAL <adilzamal@gmail.com>
2020-09-15 22:56:23 +0530

    navbar and banner completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Checkout from "./components/Checkout";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/Checkout">
+            <Navbar />
+            <Checkout />
+          </Route>
+          <Route path="/login" component={Login} />
+          <Route path="/">
+            <Navbar />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/Navbar.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <img
+          className="navbar__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt=""
+        />
+      </Link>
+      <div className="navbar__search">
+        <input type="text" className="navbar__search__input" />
+        <SearchIcon className="navbar__search__icon" />
+      </div>
+      <div className="navbar__items">
+        <Link className="navbar__items__link" to="/">
+          <div className="navbar__item">
+            <spam className="navbar__item__line1">Hello Adil</spam>
+            <spam className="navbar__item__line2">Sign In</spam>
+          </div>
+        </Link>
+        <Link className="navbar__items__link" to="/">
+          <div className="navbar__item">
+            <spam className="navbar__item__line1">Reurns</spam>
+            <spam className="navbar__item__line2">& Orders</spam>
+          </div>
+        </Link>
+        <Link className="navbar__items__link" to="/">
+          <div className="navbar__item">
+            <spam className="navbar__item__line1">Your</spam>
+            <spam className="navbar__item__line2"> Prime</spam>
+          </div>
+        </Link>
+        <Link to="/checkout" className="navbar__items__link">
+          <div className="navbar__basket">
+            <ShoppingBasketIcon />
+            <span className="navbar__item__line1 basket__count">0</span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
